0c7e86559a7a76626ee83f35dd9698c4ef871b3f
Honorisverum <bbce724e6e0864538b606e7165a6f0bfe25f16ec>
2020-06-10 10:06:35 +0300

    model development

--- models/blow.py
+++ models/blow.py
@@ -1,7 +1,22 @@
 import sys
 import torch
 
-from utils import audio
+
+def proc_problematic_samples(x,soft=True):
+    x[torch.isnan(x)]=0
+    if soft:
+        x=softclamp(x)
+    else:
+        x=torch.clamp(x,-1,1)
+    return x
+
+def softclamp(x,mx=1,margin=0.03,alpha=0.7,clipval=100):
+    x=torch.clamp(x,-clipval,clipval)
+    xabs=x.abs()
+    rmargin=mx-margin
+    mask=(xabs<rmargin).float()
+    x=mask*x+(1-mask)*torch.sign(x)*((1-torch.exp(-alpha*(xabs-rmargin)/margin))*margin+rmargin)
+    return x
 
 ########################################################################################################################
 ########################################################################################################################
@@ -50,7 +65,7 @@ class Model(torch.nn.Module):
         # Back to original dim
         h=h.squeeze(1)
         # Postproc
-        h=audio.proc_problematic_samples(h)
+        h=proc_problematic_samples(h)
         return h
 
     def precalc_matrices(self,mode):
